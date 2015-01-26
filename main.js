bc4421a5a259ddde66a8f425b4abf012d025da3f
Spencer Wyckoff <spencer.wyckoff@gmail.com>
2015-01-26 00:15:38 -0500

    finished 3 exercises

--- main.js
+++ main.js
@@ -37,15 +37,42 @@ var getAverageTime = function(person) {
 	return "Runs a marathon in an average of " + (total / races).toFixed(2) + " minutes.";
 
 	total = 0;
-
 }
 
 console.log(getAverageTime(runner1));
 console.log(getAverageTime(runner2));
 
-
 //Part 3
 
+var TimsBankAccount = {
+    checking: 0,
+    savings: 0,
+    retirement: 0
+};
+
+var RobertosBankAccount = {
+    checking: 0,
+    savings: 0,
+    retirement: 0
+};
+
+function addToBank(account, savings, retirement, checking) {
+    account.savings = account.savings + savings;
+    account.retirement = account.retirement + retirement;
+    account.checking = account.checking + checking;
+}
+
+function getSumOfAccounts(account) {
+    return (account.savings + account.checking + account.retirement);
+}
+
+addToBank(TimsBankAccount, 100, 10, 1);
+addToBank(RobertosBankAccount, 200, 50, 19);
+
+var sumOfTims = getSumOfAccounts(TimsBankAccount);
+var sumOfRobertos = getSumOfAccounts(RobertosBankAccount);
+
+console.log(sumOfTims - sumOfRobertos); // should calculate to -158
 
 
 
